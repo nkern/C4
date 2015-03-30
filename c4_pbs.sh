a9f26ead882d74e90c73abcd8daf87bf5076b040
Nicholas S. Kern <nkern@umich.edu>
2015-03-29 21:36:29 -0400

    new file:   Avg_Halo_Centers.tab 	new file:   C4_Richnesses.tab 	new file:   c4_pbs.sh

---
+++ c4_pbs.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#PBS -S /bin/sh
+#PBS -A christoq_flux
+#PBS -q flux
+#PBS -N C4_Clusters
+#PBS -l nodes=1:ppn=1,pmem=4000mb,walltime=12:00:00
+#PBS -V
+#PBS -o /nfs/christoq_ls/nkern/C4/DataSet1/
+#PBS -j oe
+#
+# Go to Working Directory
+cd /nfs/christoq_ls/nkern/C4/
+
+# Run Code 
+python c4_clusters.py
+
